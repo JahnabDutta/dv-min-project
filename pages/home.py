401276e1e77a93cb03d67ecacb97381513d9e9a5
JAHNAB DUTTA <jahnabdutta123@gmail.com>
2024-04-28 21:16:44 +0530

    added cards to homescreen
    Signed-off-by: JAHNAB DUTTA <jahnabdutta123@gmail.com>

--- pages/home.py
+++ pages/home.py
@@ -14,8 +14,89 @@ dash.register_page(
     title = "Home | Dashboard",
     path="/")
 
-layout = html.Div([
-    # Navbar(),
-    html.H1('This is our Home page'),
-    html.Div('This is our Home page content.'),
-])
+most_used_weapon = "Handgun"
+
+layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(  
+                        dbc.Card(
+                            [
+                                dbc.CardHeader("Most Used Weapon"),
+                                dbc.CardBody(
+                                    [
+                                        html.H3(f"{most_used_weapon}", className="card-title"),
+                                        html.Img(src="assets/handgun.svg", style={"width": "50px", "height": "50px"}),
+                                    ],
+                                    className="d-flex justify-content-between"
+
+                                ),
+                                dbc.CardFooter("7,105 homicides in 2016")
+                            ],
+                        ),
+                        # add padding
+                        className="py-2",
+                        width=3,
+                ),
+                dbc.Col(
+                     dbc.Card(
+                            [
+                                dbc.CardHeader("State with Highest Crime Rate"),
+                                dbc.CardBody(
+                                    [
+                                        html.H3(f"California", className="card-title"),
+                                        html.Img(src="assets/california.svg", style={"width": "50px", "height": "50px"}),
+                                    ],
+                                    className="d-flex justify-content-between"
+
+                                ),
+                                dbc.CardFooter("1,861,269 crimes in 2016")
+                            ],
+                            
+                        ),className="py-2",
+                        width=3,
+                ),
+                dbc.Col(
+                     dbc.Card(
+                            [
+                                dbc.CardHeader("Crime with Highest Rate"),
+                                dbc.CardBody(
+                                    [
+                                        html.H3(f"Property Crime", className="card-title"),
+                                        html.Img(src="assets/theft.svg", style={"width": "50px", "height": "50px"}),
+                                    ],
+                                    className="d-flex justify-content-between"
+
+                                ),
+                                dbc.CardFooter("7,993,631 incidents in 2016")
+                            ],
+                            
+                        ),className="py-2",
+                        width=3,
+                ),
+                dbc.Col(
+                     dbc.Card(
+                            [
+                                dbc.CardHeader("State with Highest Law Enforcement"),
+                                dbc.CardBody(
+                                    [
+                                        html.H3(f"California ", className="card-title"),
+                                        html.Img(src="assets/california.svg", style={"width": "50px", "height": "50px"}),
+                                    ],
+                                    className="d-flex justify-content-between"
+
+                                ),
+                                dbc.CardFooter("78,062 officers")
+                            ],
+                            
+                        ),className="py-2",
+                        width=3,
+                )
+            ],
+            # add style such that the height of the card is 100% of the container
+            style={"height": "100%"}
+        )
+    ],
+    fluid=True
+)
